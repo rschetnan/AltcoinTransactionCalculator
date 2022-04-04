d775913c43f25be1573e3aae1270540e816eb29e
rschetnan <48975816+rschetnan@users.noreply.github.com>
2022-04-03 23:57:28 -0600

    Changes to be committed: 	modified:   altcoin_transaction_calculator.py

--- altcoin_transaction_calculator.py
+++ altcoin_transaction_calculator.py
@@ -52,89 +52,92 @@ if header != None:
         #Add the list of transaction records dictionaryies to the transactions dictionary
         transactions[transaction_id] = transaction_records
 
-tx_id = input("Provide the transaction ID:")
+while True:
 
-if len(tx_id) == 0: sys.exit()
+    tx_id = input("Provide the transaction ID:")
 
-if tx_id in transactions:
-    tx_records = transactions[tx_id]
-else:
-    print('Transaction not found!')
-    sys.exit()
+    if len(tx_id) == 0: sys.exit()
 
-tx_record_count = len(tx_records)
+    if tx_id in transactions:
+        tx_records = transactions[tx_id]
+    else:
+        print('Transaction not found!')
+        sys.exit()
 
-#Display a menu of records associated with the transaction
-print('Transaction records:')
-print()
-for i in range(tx_record_count):
-   print(i, ":", tx_records[i]['Value'], tx_records[i]['TokenName']) 
+    tx_record_count = len(tx_records)
 
-#Ask user to select a record
-print()
-tx_record_input = input('Select a record or Enter for 0: ')
+    #Display a menu of records associated with the transaction
+    print('Transaction records:')
+    print()
+    for i in range(tx_record_count):
+        print(i, ":", tx_records[i]['Value'], tx_records[i]['TokenName']) 
 
-try:
-    record_index = int(tx_record_input)
-except:
-    record_index = 0
+    #Ask user to select a record
+    print()
+    tx_record_input = input('Select a record or Enter for 0: ')
 
-#Grab the selected transaction record
-tx = tx_records[record_index]
-
-tx_date = tx['Date']
-contract_address = tx['ContractAddress']
-
-#Display the contract address and give user the opportunity to change it
-print('Contract address is', contract_address, 'for', tx['TokenName'])
-
-contract_address_input = input("Provide a new contract address or Enter:")
-
-if len(contract_address_input) > 0:
-    contract_address = contract_address_input.lower()
-    print('New contract address is', contract_address)
-
-#look up the price in the prices dictionary and provide status messages
-if contract_address in js:
-    prices = js[contract_address]
-else:
-    print('No prices found for this token!')
-    prices = {}
-
-if tx_date in prices:
-    price = float(prices[tx_date])
-else:
-    print('No price found for date of transaction!')
-    price = 0.0
-
-print('Token price:', price)
-
-# Give the user an opportunity to change the price
-price_input = input("Provide a new token price or Enter:")
-if len(price_input) > 0:
     try:
-        price = float(price_input)
+        record_index = int(tx_record_input)
     except:
-        print('Price is not valid!')
+        record_index = 0
+
+    #Grab the selected transaction record
+    tx = tx_records[record_index]
+
+    tx_date = tx['Date']
+    contract_address = tx['ContractAddress']
+
+    #Display the contract address and give user the opportunity to change it
+    print('Contract address is', contract_address, 'for', tx['TokenName'])
+
+    contract_address_input = input("Provide a new contract address or Enter:")
+
+    if len(contract_address_input) > 0:
+        contract_address = contract_address_input.lower()
+        print('New contract address is', contract_address)
+
+    #look up the price in the prices dictionary and provide status messages
+    if contract_address in js:
+        prices = js[contract_address]
+    else:
+        print('No prices found for this token!')
+        prices = {}
+
+    if tx_date in prices:
+        price = float(prices[tx_date])
+    else:
+        print('No price found for date of transaction!')
         price = 0.0
-    print('New price is', price)
 
-token_amount = tx['Value']
+    print('Token price:', price)
 
-#Show the token oamout and give the user a chance to update it
-print('Token amount:', token_amount)
-token_amount_input = input("Provide a new token amount or Enter:")
-if len(token_amount_input) > 0:
-    try:
-        token_amount = float(token_amount_input)
-    except:
-        print('Token amount is not valid!')
-        token_amount = 0.0
-    print('New token amount is', token_amount)
+    # Give the user an opportunity to change the price
+    price_input = input("Provide a new token price or Enter:")
+    if len(price_input) > 0:
+        try:
+            price = float(price_input)
+        except:
+            print('Price is not valid!')
+            price = 0.0
+        print('New price is', price)
 
-#Calculate and display the transaction value
-tx_value = price * token_amount
-print('Transaction value is', tx_value)
+    token_amount = tx['Value']
+
+    #Show the token oamout and give the user a chance to update it
+    print('Token amount:', token_amount)
+    token_amount_input = input("Provide a new token amount or Enter:")
+    if len(token_amount_input) > 0:
+        try:
+            token_amount = float(token_amount_input)
+        except:
+            print('Token amount is not valid!')
+            token_amount = 0.0
+        print('New token amount is', token_amount)
+
+    #Calculate and display the transaction value
+    tx_value = price * token_amount
+    print('Transaction value is', tx_value)
+    print()
 
 
 
